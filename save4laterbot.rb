6fd98676cc565838122636bd858c156e8b0d6d59
Júlio Campos <jcserracampos@gmail.com>
2018-01-14 21:18:33 -0200

    Now it's working. I forgot to interpolate string. I need some coffee

--- save4laterbot.rb
+++ save4laterbot.rb
@@ -7,15 +7,16 @@ else
     token = File.read(".telegram_token").strip
 end
 
-db = PG.connect(ENV["DATABASE_URL"])
+# db = PG.connect(ENV["DATABASE_URL"])
+db = PG.connect("postgres://hcxrdxyfsnsiew:30223d2dae076e7fc6cda7efd321981a071cf15ead5987b1987edbde200cb5cb@ec2-54-221-251-195.compute-1.amazonaws.com:5432/d2pinu54s5arm0")
 db.exec "CREATE TABLE IF NOT EXISTS Links(Link VARCHAR(255), Author VARCHAR(30), Chat VARCHAR(30))"
 
 Telegram::Bot::Client.run(token) do |bot|
     bot.listen do |message|
         links = message.text.split(/\s+/).find_all { |u| u =~ /^https?:/ }
 
-        links.each do |msg|
-            db.exec "INSERT INTO Links VALUES(#{msg.text}, #{msg.from.username}, #{msg.chat.title})"
+        links.each do |link|
+            db.exec "INSERT INTO Links VALUES('#{link}', '#{message.from.username}', '#{message.chat.title}')"
         end
       
     end
